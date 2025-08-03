c0e6460d4cb477b12a7b041e532b39d019e423fe
Sarthak <sarthakkulkarni612@gmail.com>
2025-08-03 17:32:26 +0530

    test-1

--- app/page.tsx
+++ app/page.tsx
@@ -17,12 +17,12 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        {/* <Grid />
         <RecentProjects />
         <Clients />
         <Experience />
-        <Approach />
-        <Footer />
+        <Approach />*/}
+        <Footer /> 
       </div>
     </main>
   );
